6095336c98b29eec350f1b33a8fef390dd0b246a
Marina Lugovskaya <anomaliya1993@gmail.com>
2021-09-05 15:10:35 +0300

    new2

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,7 +1,14 @@
-export default function FeedbackOptions ({ options, onLeaveFeedback }) {
-    return options.map(option => (
-      <button type="button" key={option} onClick={() => onLeaveFeedback(option)}>
-        {option}
-      </button>
-    ));
-  };
+import PropTypes from 'prop-types';
+
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  return options.map(option => (
+    <button type="button" key={option} onClick={() => onLeaveFeedback(option)}>
+      {option}
+    </button>
+  ));
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.number,
+  onLeaveFeedback: PropTypes.func,
+};
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,14 @@
-export default function Section ({title, children}) {
+import PropTypes from 'prop-types';
+
+export default function Section({ title, children }) {
   return (
     <Section>
       <h1>{title}</h1>
       {children}
     </Section>
-  )
+  );
+}
+
+Section.propTypes = {
+  title: PropTypes.string,
 };
